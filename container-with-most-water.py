61b7423a025a04f7863c7ddb046924c2ca81d214
MuzaffarSoliyev <muzaffar.soliyev97@gmail.com>
2022-02-25 16:22:49 +0500

    added container with most water

---
+++ container-with-most-water.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height) - 1
+
+        area, max_area = 0, 0
+
+        while l < r:
+            area = (r - l) * min(height[l], height[r])
+            max_area = max(max_area, area)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
